79027a2ba32a5dc49ebfda8b93b16d5f8524567f
Beqa Tsiramua <97254843+beqatsiramua1@users.noreply.github.com>
2022-08-09 08:31:31 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let myInfo = {
+  firstName: "Beqa",
+  lastName: "Tsirmaua",
+  age: 21,
+  city: "Tbilisi",
+  email: "beka.tsiramua.1@iliauni.edu.ge",
+  isMarried: false,
+  programmingLanguage: "JavaScript",
+  bestFramework: "Bootstrap"
+}
+
+console.log(myInfo.firstName)
+console.log(myInfo.lastName)
+console.log(myInfo.age)
+console.log(myInfo.city)
+console.log(myInfo.email)
+console.log(myInfo.isMarried)
+console.log(myInfo.programmingLanguage)
+console.log(myInfo.bestFramework)
